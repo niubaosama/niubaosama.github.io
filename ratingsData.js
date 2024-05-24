a1c7e0a9ffd1adf9dc89f6601a439e8d08bad79c
牛瑞洲 <754501685@qq.com>
2024-05-24 16:25:01 +0800

    0.01

---
+++ ratingsData.js
@@ -0,0 +1,24 @@
+// ratingsData.js
+const ratings = {
+    rating1: 1,
+    rating2: 1,
+    rating3: 2,
+    rating4: 0,
+    rating5: 0,
+    rating6: 0,
+    rating7: 0,
+    rating8: 0,
+    rating9: 0,
+    rating10: 0,
+    rating11: 0,
+    rating12: 0,
+    rating13: 1,
+    rating14: 1,
+    rating15: 0,
+    rating16: 0,
+    rating17: 0,
+    rating18: 0,
+    rating19: 0,
+};
+
+export default ratings;
